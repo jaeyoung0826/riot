b8bffc6510fad6c5197841a21d8b6c1ec293ec61
jaeyoung08 <jae_young_97@khu.ac.kr>
2020-06-28 20:06:18 +0900

    app.js modified

--- RUTROLL/app.js
+++ RUTROLL/app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var app = express();
-var router = require('./router/main')(app);
+var router = require('./router/main');
 var bodyParser = require('body-parser');
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
-
+app.use('/',router);
 var server = app.listen(3000, function(){
 	console.log('good');
 });
